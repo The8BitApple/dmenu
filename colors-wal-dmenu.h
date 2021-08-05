1b2176b786f848c6ffbb0f6c2d2c24e7a154de95
The8BitApple <stefanharris16@gmail.com>
2021-08-05 21:37:28 +0100

    Integrated Pywal theme. Moved to root for convenience

---
+++ colors-wal-dmenu.h
@@ -0,0 +1,6 @@
+static const char *colors[SchemeLast][2] = {
+	/*     fg         bg       */
+	[SchemeNorm] = { "#f4ecc5", "#0a0007" },
+	[SchemeSel] = { "#f4ecc5", "#B35E53" },
+	[SchemeOut] = { "#f4ecc5", "#C6A596" },
+};
